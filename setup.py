a36c2aec388d0840111d1812cc36ced5c11bf78a
Debin <luodeb@outlook.com>
2023-07-01 22:37:02 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/luodeb/lcmtools'
 EMAIL = 'luodeb@qq.com'
 AUTHOR = 'Luo Debin'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
